5ee858651dd068daa605f1882c888c251c3c9f4d
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-01-15 23:22:28 +0000

    add canvas

--- pong/script.js
+++ pong/script.js
@@ -1,7 +1,7 @@
 // Canvas
 const { body } = document;
-
-
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
 const width = 500;
 const height = 700;
 const screenWidth = window.screen.width;
