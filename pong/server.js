45b51926419708c3ae5aef78b09749c777b9af26
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-01-17 23:15:41 +0000

    setting up a socket.io server

---
+++ pong/server.js
@@ -0,0 +1,10 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+const PORT = 3000;
+server.listen(PORT);
+console.log(`Listeing on port ${PORT}...`);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
