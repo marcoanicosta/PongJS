22e212d09da1b8223d96a42aeb39697def7c3751
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-01-19 15:53:30 +0000

    listening for events in the pong server

--- pong/server.js
+++ pong/server.js
@@ -1,3 +1,5 @@
+const { log } = require('console');
+
 const server = require('http').createServer();
 const io = require('socket.io')(server, {
    cors: {
@@ -10,6 +12,18 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listeing on port ${PORT}...`);
 
+let playerReadyCount = 0;
+
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
+
+    socket.on('ready', ()=> {
+        console.log('Player Ready', socket.id);
+
+        playerReadyCount++;
+
+        if(playerReadyCount==2){
+            //broadcast
+        }
+    });
 });
