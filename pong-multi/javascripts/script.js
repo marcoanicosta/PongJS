768fb94784152f9599f5143d17e33340c23f5305
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-01-21 21:47:45 +0000

    refactor for 2 player mode

--- pong-multi/javascripts/script.js
+++ pong-multi/javascripts/script.js
@@ -1,7 +1,9 @@
 // Canvas Related 
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
+const socket = io('http://localhost:3000');
 let paddleIndex = 0;
+let isReferee = false;
 
 let width = 500;
 let height = 700;
@@ -19,11 +21,11 @@ let ballX = 250;
 let ballY = 350;
 let ballRadius = 5;
 let ballDirection = 1;
-
+ 
 // Speed
 let speedY = 2;
 let speedX = 0;
-let computerSpeed = 4;
+
 
 // Score for Both Players
 let score = [ 0, 0 ];
@@ -37,17 +39,17 @@ function createCanvas() {
   renderCanvas();
 }
 
-// Wait for Opponents
-// function renderIntro() {
-//   // Canvas Background
-//   context.fillStyle = 'black';
-//   context.fillRect(0, 0, width, height);
+//Wait for Opponents
+function renderIntro() {
+  // Canvas Background
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, width, height);
 
-//   // Intro Text
-//   context.fillStyle = 'white';
-//   context.font = "32px Courier New";
-//   context.fillText("Waiting for opponent...", 20, (canvas.height / 2) - 30);
-// }
+  // Intro Text
+  context.fillStyle = 'white';
+  context.font = "32px Courier New";
+  context.fillText("Waiting for opponent...", 20, (canvas.height / 2) - 30);
+}
 
 // Render Everything on Canvas
 function renderCanvas() {
@@ -146,35 +148,14 @@ function ballBoundaries() {
       trajectoryX[1] = ballX - (paddleX[1] + paddleDiff);
       speedX = trajectoryX[1] * 0.3;
     } else {
-      // Reset Ball, Increase Computer Difficulty, add to Player Score
-      if (computerSpeed < 6) {
-        computerSpeed += 0.5;
-      }
       ballReset();
       score[0]++;
     }
   }
 }
 
-// Computer Movement
-function computerAI() {
-  if (playerMoved) {
-    if (paddleX[1] + paddleDiff < ballX) {
-      paddleX[1] += computerSpeed;
-    } else {
-      paddleX[1] -= computerSpeed;
-    }
-    if (paddleX[1] < 0) {
-      paddleX[1] = 0;
-    } else if (paddleX[1] > (width - paddleWidth)) {
-      paddleX[1] = width - paddleWidth;
-    }
-  }
-}
-
 // Called Every Frame
 function animate() {
-  computerAI();
   ballMove();
   renderCanvas();
   ballBoundaries();
@@ -182,11 +163,14 @@ function animate() {
 }
 
 // Start Game, Reset Everything
-function startGame() {
+function loadGame() {
   createCanvas();
-  // renderIntro();
-  
-  paddleIndex = 0;
+  renderIntro();
+  socket.emit('ready');
+}
+
+function startGame() {
+  paddleIndex = isReferee ? 0 : 1;
   window.requestAnimationFrame(animate);
   canvas.addEventListener('mousemove', (e) => {
     playerMoved = true;
@@ -203,5 +187,16 @@ function startGame() {
 }
 
 // On Load
-startGame();
+loadGame();
 
+socket.on('connect', () => {
+  console.log('connected as..', socket.id);
+})
+
+socket.on('startGame', (refereeID) => {
+  console.log('Referee is', refereeID);
+
+  isReferee = socket.id === refereeID;
+  
+  startGame();
+});;
