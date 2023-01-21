9d39be65cd601b7f62eb0371b3b65aaf78b4b9cf
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-01-21 21:08:54 +0000

    refactor folders

---
+++ pong-multi/server.js
@@ -0,0 +1,29 @@
+const { log } = require('console');
+
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+   cors: {
+   origin: '*',
+   methods: ['GET', 'POST']
+ }
+});
+
+const PORT = 3000;
+server.listen(PORT);
+console.log(`Listeing on port ${PORT}...`);
+
+let playerReadyCount = 0;
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('ready', ()=> {
+        console.log('Player Ready', socket.id);
+
+        playerReadyCount++;
+
+        if(playerReadyCount==2) {
+            io.emit('startGame', socket.id);
+        }
+    });
+});
